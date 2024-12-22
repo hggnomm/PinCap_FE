981e37795b8a5ab1cb0fd5645d45b1b1fd44354a
hggnomm <hggnomm.dev@gmail.com>
2024-12-22 18:33:47 +0700

    fix code hover

--- src/pages/PinCap/PinMedia/PinMedia.tsx
+++ src/pages/PinCap/PinMedia/PinMedia.tsx
@@ -15,11 +15,11 @@ const PinMedia = (props: any) => {
     <motion.div
       className="box"
       onClick={() => openDetailMedia(props.data?.id)}
-      style={{ position: "relative", overflow: "hidden" }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 1 }}
+      style={{ position: "relative", overflow: "hidden" }}
     >
       {isMp4 ? (
         <video controls>
@@ -36,13 +36,12 @@ const PinMedia = (props: any) => {
           top: 0,
           left: 0,
           right: 0,
-          bottom: 0,
+          bottom: 3,
           backgroundColor: "rgba(0, 0, 0, 0.4)",
           opacity: 0,
           transition: "opacity 0.3s",
           zIndex: 1,
           borderRadius: "15px",
-          height: "100%",
         }}
       />
     </motion.div>
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Register } from './Auth/Register'
-export { default as Login } from './Auth/Login'
-export { default as Header } from '../components/header/HeaderCommon'
-export { default as Home } from './Home/Home'
-
-
-
-
