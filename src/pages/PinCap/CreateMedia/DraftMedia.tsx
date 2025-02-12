eeaadac7bdfed686ce55d810170fd9bd1ed82724
hggnomm <hggnomm.dev@gmail.com>
2025-02-13 00:50:44 +0700

    update delete draft part 1

--- src/pages/PinCap/CreateMedia/DraftMedia.tsx
+++ src/pages/PinCap/CreateMedia/DraftMedia.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import { getDetailMedia, getMyMedias } from "../../../api/media";
-import Loading from "../../../components/loading/Loading";
+import React, { useState } from "react";
+import { getDetailMedia } from "../../../api/media";
 import { Divider } from "antd/es";
 import { Media } from "type";
+import ButtonCircle from "../../../components/buttonCircle/ButtonCircle";
+import { EllipsisOutlined } from "@ant-design/icons/lib";
+import ModalComponent from "../../../components/modal/ModalComponent";
+import { toast } from "react-toastify";
 
 interface DraftMediaProps {
   resetFormAndCloseDrawer: () => void;
@@ -18,13 +21,38 @@ const DraftMedia = ({
   loadingDrafts,
 }: DraftMediaProps) => {
   const [isSelectedMedia, setIsSelectedMedia] = useState<Media | null>(null);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+  const [detailMedia, setDetailMedia] = useState<Media | null>(null);
 
   const handleMediaClick = async (media: Media) => {
-    setIsSelectedMedia(media);
-    const detailMedia = await getDetailMedia(media.id);
+    if (isSelectedMedia?.id !== media.id) {
+      setIsSelectedMedia(media);
+    }
+    const fetchedDetailMedia = await getDetailMedia(media.id);
 
-    if (detailMedia) {
-      onSelectMedia(detailMedia);
+    if (fetchedDetailMedia) {
+      setDetailMedia(fetchedDetailMedia);
+      onSelectMedia(fetchedDetailMedia);
+    }
+  };
+
+  const handleDeleteClick = (media: Media) => {
+    setDetailMedia(media);
+    onSelectMedia(media);
+    setDeleteModalVisible(true);
+  };
+
+  const deleteDraft = async () => {
+    try {
+      console.log("Deleting draft...");
+
+      setDeleteModalVisible(false);
+      toast.success("Draft deleted successfully!"); 
+    } catch (error) {
+      console.error("Error deleting album:", error);
+      toast.error(
+        "An error occurred while deleting the album. Please try again."
+      );
     }
   };
 
@@ -61,9 +89,55 @@ const DraftMedia = ({
                   />
                 </div>
                 <div className="media-name">{media.media_name}</div>
+                <div className="options">
+                  <ButtonCircle
+                    text="Options"
+                    paddingClass="padding-small"
+                    icon={<EllipsisOutlined style={{ fontSize: "26px" }} />}
+                    dropdownMenu={[
+                      {
+                        key: "1",
+                        title: "Delete",
+                        onClick: () => handleDeleteClick(media),
+                      },
+                    ]}
+                  />
+                </div>
               </div>
             ))}
           </div>
+
+          <ModalComponent
+            titleDefault="Delete this draft?"
+            visible={deleteModalVisible}
+            onCancel={() => setDeleteModalVisible(false)}
+            onConfirm={deleteDraft}
+            buttonLabels={{
+              confirmLabel: "Delete",
+              cancelLabel: "Cancel",
+            }}
+          >
+            <div
+              style={{
+                marginBottom: 20,
+                marginTop: 20,
+              }}
+            >
+              Are you sure you want to delete this draft
+              <p
+                style={{
+                  fontWeight: 500,
+                  fontSize: "1.1em",
+                  display: "inline",
+                  marginRight: "5px",
+                  marginLeft: "5px",
+                }}
+              >
+                {detailMedia?.media_name}
+              </p>
+              This action cannot be undone.
+            </div>
+          </ModalComponent>
         </>
       )}
     </div>
